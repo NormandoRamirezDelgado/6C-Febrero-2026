bb69443ed54821db1ff62a17e2bf92d6443de048
Normando Ramírez <nramirezd@cbtis236.edu.mx>
2026-02-06 09:38:51 -0600

    Operadores

---
+++ 5_operadores_aritmeticos.dart
@@ -0,0 +1,13 @@
+void main() {
+  //Operadores Aritméticos
+  int numeroUno = 10;
+  int numeroDos = 3;
+
+  print(numeroUno + numeroDos);    // Suma: 13
+  print(numeroUno - numeroDos);    // Resta: 7
+  print(numeroUno * numeroDos);    // Multiplicación: 30
+  print(numeroUno / numeroDos);    // División: 3.333...
+  print(numeroUno ~/ numeroDos);   // División entera: 3
+  print(numeroUno % numeroDos);    // Módulo: 1
+
+}
---
+++ 6_operadores_asignacion.dart
@@ -0,0 +1,16 @@
+void main() {
+  //Operadores de Asignación
+  int numero = 17;
+  print(numero);
+
+  numero += 5;    // numero = numero + 5  → numero = 15
+  print(numero);
+  numero -= 3;    // numero = numero - 3  → numero = 12
+  print(numero);
+  numero *= 2;    // numero = numero * 2  → numero = 24
+  print(numero);
+  numero ~/= 4;   // numero = numero ~/ 4 → numero = 6
+  print(numero);
+
+
+}
