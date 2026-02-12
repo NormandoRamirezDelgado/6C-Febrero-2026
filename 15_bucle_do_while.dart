58bb1e35fbb82ee2751dcb5e327ed949bc167949
Normando Ramírez <nramirezd@cbtis236.edu.mx>
2026-02-12 08:15:09 -0600

    Do While

---
+++ 15_bucle_do_while.dart
@@ -0,0 +1,19 @@
+void main() {
+  
+  //Do While
+  print('\nCiclo Do While Incremento');
+  int numero = 1;
+  do {
+    print('Número: $numero');
+    numero += 2;
+  } while (numero <= 5);
+  // Salida: Número: 1, 2, 3
+  
+  //Do While Incrementos Diferentes
+  print('\nCiclo Do While Incremento');
+  numero = 0;
+  do {
+    print('Número: $numero');
+    numero += 2;
+  } while (numero <= 5);
+}
